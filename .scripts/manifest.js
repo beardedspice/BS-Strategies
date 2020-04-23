90b8e29d3b0004dd3a4b51c5c3935efff41f93a3
Roman Sokolov <calm@adguard.com>
2020-04-23 23:49:18 +0300

    added manifest

---
+++ .scripts/manifest.js
@@ -0,0 +1,25 @@
+
+var fs = require('fs');
+var path = require('path');
+
+const BASEPATH =  path.dirname(__dirname)+'/';
+const MANIFEST = BASEPATH+'manifest.json';
+const STRATEGY_EXT = '.bsstrategy';
+
+try {
+    let out = {};
+    fs.readdirSync(BASEPATH).forEach(el => {
+        if (path.extname(el) == STRATEGY_EXT) {
+            try {
+                let stg = fs.readFileSync(el, 'utf8');
+                const obj = eval(stg);
+                out[path.basename(el, STRATEGY_EXT)] = {'version': obj.version, 'name': obj.displayName};
+            } catch (error) {
+                console.error(error)
+            }
+        }
+    });    
+    fs.writeFileSync(MANIFEST,JSON.stringify(out));
+} catch (error) {
+    console.error(error)
+}
